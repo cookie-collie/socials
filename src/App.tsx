3d3f4189438be6d2de8e55e217c4797cd11e193c
Cookie Collie <ediblechocolatething@gmail.com>
2023-07-12 01:12:31 +0700

    add: Add main page (barebone)

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
+import { ConfigProvider } from "antd"
+import MainLayout from "./components/MainLayout"
+
 function App() {
+    const _theme = {
+        token: {
+            colorTextBase: "#44464f",
+            fontSize: 16,
+            borderRadius: 6,
+            wireframe: false,
+            // colorPrimary: "#6b8fdf",
+        },
+    }
+
     return (
-        <div className="App">
-      
-        </div>
+        <ConfigProvider theme={_theme}>
+            <MainLayout><p>Body</p></MainLayout>
+        </ConfigProvider>
+        // <MainLayout><p>Body</p></MainLayout>
+
     )
 }
 
