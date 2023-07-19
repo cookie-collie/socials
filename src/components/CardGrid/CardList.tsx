19de2294a11c1255c3319dfc82189be98cb1fd8d
Cookie Collie <ediblechocolatething@gmail.com>
2023-07-20 01:28:59 +0700

    feat: Add animations for price cards (WIP)

--- src/components/CardGrid/CardList.tsx
+++ src/components/CardGrid/CardList.tsx
@@ -6,13 +6,19 @@ import {
     CardProps,
     Heading,
     Image,
+    Img,
     LinkBox,
     LinkOverlay,
+    Modal,
+    ModalContent,
+    ModalOverlay,
     Stack,
     Stat,
     StatHelpText,
     StatNumber,
 } from "@chakra-ui/react"
+import { AnimatePresence, motion, useAnimation } from "framer-motion"
+import { useState } from "react"
 
 export interface CardListProps {
     heading?: React.ReactNode
@@ -21,6 +27,7 @@ export interface CardListProps {
     linkTo?: string
     price?: string
     additionalInfo?: React.ReactNode
+    id: string
 }
 
 export const CardList = (props: {
@@ -28,50 +35,113 @@ export const CardList = (props: {
     variant: CardProps["variant"]
 }) => {
     const { cardContent, variant } = props
+
+    const [selectedCard, setSelectedCard] = useState("")
+
+    const _controls = useAnimation()
+    const _variants = {
+        hover: { scale: 1.05 },
+        initial: { scale: 1 },
+    }
     return (
         <>
             {cardContent.map((card, i) => (
-                <LinkBox
+                <motion.div
                     key={"card-" + i}
-                    as={Card}
-                    variant={variant}
-                    overflow={"hidden"}
+                    variants={_variants}
+                    onMouseEnter={() => _controls.start("hover")}
+                    onMouseLeave={() => _controls.start("initial")}
+                    layoutId={card.id}
                 >
-                    <AspectRatio maxH={"200px"}>
-                        <Image src={card.coverImgUrl} />
-                    </AspectRatio>
-                    <LinkOverlay
-                        href={card.linkTo}
-                        target="_blank"
-                        rel="noopener noreferrer"
+                    <LinkBox
+                        as={Card}
+                        variant={variant}
+                        overflow={"hidden"}
+                        minH={"100%"}
                     >
-                        <CardHeader>
-                            <Stack gap={3}>
-                                {typeof card.heading === typeof "" ? (
-                                    <Heading
-                                        fontFamily={"Fredoka, Comfortaa, Arial"}
-                                        size={"lg"}
-                                        color={"blackAlpha.800"}
-                                        textAlign={"center"}
-                                    >
-                                        {card.heading}
-                                    </Heading>
-                                ) : (
-                                    <>{card.heading}</>
-                                )}
+                        <motion.div variants={_variants} animate={_controls}>
+                            <AspectRatio maxH={"200px"}>
+                                <Image src={card.coverImgUrl} />
+                            </AspectRatio>
+                        </motion.div>
 
-                                <Stat textAlign={"center"}>
-                                    <StatNumber>{card.price}</StatNumber>
-                                    <StatHelpText>
-                                        {card.additionalInfo}
-                                    </StatHelpText>
-                                </Stat>
-                            </Stack>
-                        </CardHeader>
-                    </LinkOverlay>
-                    <CardBody>{card.body}</CardBody>
-                </LinkBox>
+                        <LinkOverlay
+                            // href={card.linkTo}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <CardHeader>
+                                <Stack gap={3}>
+                                    {typeof card.heading === typeof "" ? (
+                                        <Heading
+                                            fontFamily={
+                                                "Fredoka, Comfortaa, Arial"
+                                            }
+                                            size={"lg"}
+                                            color={"blackAlpha.800"}
+                                            textAlign={"center"}
+                                        >
+                                            {card.heading}
+                                        </Heading>
+                                    ) : (
+                                        <>{card.heading}</>
+                                    )}
+
+                                    <Stat textAlign={"center"}>
+                                        <StatNumber>{card.price}</StatNumber>
+                                        <StatHelpText>
+                                            {card.additionalInfo}
+                                        </StatHelpText>
+                                    </Stat>
+                                </Stack>
+                            </CardHeader>
+                        </LinkOverlay>
+                        <CardBody>{card.body}</CardBody>
+                    </LinkBox>
+                </motion.div>
             ))}
+
+            <AnimatePresence>
+                <Modal
+                    isOpen={selectedCard ? true : false}
+                    onClose={() => setSelectedCard("")}
+                    size={"4xl"}
+                >
+                    <ModalOverlay />
+                    <ModalContent bg={"transparent"}>
+                        {selectedCard && (
+                            <motion.div layoutId={selectedCard}>
+                                {cardContent.map(
+                                    (item) =>
+                                        item.id === selectedCard && (
+                                            <Card key={item.id}>
+                                                <Img
+                                                    src={item.coverImgUrl}
+                                                    maxH={"300px"}
+                                                    objectFit={"contain"}
+                                                    style={{}}
+                                                />
+                                                <CardHeader>
+                                                    <Heading
+                                                        fontFamily={
+                                                            "Fredoka, Comfortaa, Arial"
+                                                        }
+                                                        size={"lg"}
+                                                        color={"blackAlpha.800"}
+                                                        textAlign={"center"}
+                                                    >
+                                                        {item.heading}
+                                                    </Heading>
+                                                </CardHeader>
+                                                <CardBody>{item.body}</CardBody>
+                                            </Card>
+                                        )
+                                )}
+                            </motion.div>
+                        )}
+                    </ModalContent>
+                </Modal>
+            </AnimatePresence>
         </>
     )
 }
