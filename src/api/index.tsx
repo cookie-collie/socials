611c85ee20ef4ca0b2efa2c3d85959775089f123
Cookie Collie <ediblechocolatething@gmail.com>
2023-07-24 03:32:31 +0700

    feat: Finish API for submitting form

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,7 +1,29 @@
-import axios from "axios"
+import axios, { AxiosError } from "axios"
+
+const DEV_URL = process.env.REACT_APP_DEV_API_URL
 
 export function testAPI() {
-    return axios.get(process.env.REACT_APP_API_URL!).then((res) => {
+    return axios.get(`${DEV_URL}`).then((res) => {
         console.log(res)
     })
+}
+
+export function testSend(form: any) {
+    return axios.post(`${DEV_URL}/test-send`, form)
+}
+
+export async function sendRequestForm(
+    form: any,
+    setStatus?: (status: string) => void
+) {
+    return await axios
+        .post(`${DEV_URL}/api/request`, form)
+        .then((res) => {
+            setStatus?.("OK")
+            console.log(res)
+        })
+        .catch((error: AxiosError | Error) => {
+            setStatus?.("Error")
+            console.log("Error: ", error.message)
+        })
 }
--- src/fragments/index.tsx
+++ src/fragments/index.tsx
@@ -1,5 +1,6 @@
 import { AboutWebsite } from "./AboutWebsite"
 import { CommissionDetails } from "./CommissionDetails"
+import { CustomAlert } from "./CustomAlertDialog"
 import { Extras } from "./Extras"
 import { FormGuide } from "./FormGuide"
 import { PriceSheet } from "./PriceSheet"
@@ -9,6 +10,7 @@ import { TOS } from "./TOS"
 export {
     AboutWebsite,
     CommissionDetails,
+    CustomAlert,
     Extras,
     FormGuide,
     PriceSheet,
